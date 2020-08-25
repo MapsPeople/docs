8fe8745604096c154f05719054d8fc52a2c5dff7
Mark Hougaard Jensen <mark@marks.dk>
2020-08-25 13:43:05 +0200

    chore: Add css-files to watch target

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,13 @@ const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
 const embedYouTube = require('eleventy-plugin-youtube-embed');
 
 module.exports = function (eleventyConfig) {
-  // Copy './assets/' to '_site/assets/'
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(embedYouTube);
   eleventyConfig.addPassthroughCopy('src/assets');
   eleventyConfig.addPassthroughCopy({
     './node_modules/@mapsindoors/components/': 'assets/components',
   });
-
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addWatchTarget('./src/assets/');
   eleventyConfig.setBrowserSyncConfig({
     ui: false,
   });
